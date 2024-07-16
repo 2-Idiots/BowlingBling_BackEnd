33a5213d85f6c7260599d443942b0bae8c5cfa56
JiHwan BAE <jimil0902@naver.com>
2024-07-16 11:15:18 +0900

    [ADD] 환경 설정2 기본 엔티티 설정

---
+++ src/test/java/com/capstone/bowlingbling/BowlingBlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.bowlingbling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingBlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
