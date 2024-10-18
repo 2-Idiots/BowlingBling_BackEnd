44471711f7d0160b181654e6ad33742e7cef13a9
JiHwan BAE <jimil0902@naver.com>
2024-10-18 10:11:51 +0900

    [UPDATE] 레슨 댓글, 볼링장 댓글에 대한 페이징 기능 수정 및 swagger 설명문 작성

--- src/main/java/com/capstone/bowlingbling/domain/comment/controller/CenterCommentController.java
+++ src/main/java/com/capstone/bowlingbling/domain/comment/controller/CenterCommentController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,12 +24,12 @@ public class CenterCommentController {
 
     @Operation(summary = "댓글 조회", description = "특정 볼링장에 달린 모든 댓글을 조회합니다. 한 번에 10개씩 조회됩니다.")
     @GetMapping
-    public ResponseEntity<Page<CommentResponseDto>> getComments(@PathVariable Long centersId) {
-        Page<CommentResponseDto> comments = centerCommentService.getAllComments(centersId, Pageable.ofSize(10));
+    public ResponseEntity<Page<CommentResponseDto>> getComments(@PathVariable Long centersId, @RequestParam(defaultValue = "0") int page) {
+        Page<CommentResponseDto> comments = centerCommentService.getAllComments(centersId, PageRequest.of(page, 10));
         return ResponseEntity.ok(comments);
     }
 
-    @Operation(summary = "댓글 작성", description = "특정 레슨에 댓글을 작성합니다.")
+    @Operation(summary = "댓글 작성", description = "특정 볼링장에 댓글을 작성합니다.")
     @PostMapping("/save")
     public ResponseEntity<String> saveComment(@PathVariable Long centersId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal User user) {
         String memberEmail = user.getUsername();
@@ -36,19 +37,19 @@ public class CenterCommentController {
         return ResponseEntity.ok("댓글 저장 성공");
     }
 
-    @Operation(summary = "댓글 수정", description = "특정 레슨 댓글을 수정합니다.")
+    @Operation(summary = "댓글 수정", description = "특정 볼링장 댓글을 수정합니다.")
     @PatchMapping("/{commentId}/update")
     public ResponseEntity<String> updateComment(@PathVariable Long centersId, @PathVariable Long commentId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal User user) {
         String memberEmail = user.getUsername();
         centerCommentService.updateComment(centersId, commentId, requestDto, memberEmail);
-        return ResponseEntity.ok("레슨 수정 성공");
+        return ResponseEntity.ok("댓글 수정 성공");
     }
 
-    @Operation(summary = "댓글 삭제", description = "특정 마켓 댓글을 삭제합니다.")
+    @Operation(summary = "댓글 삭제", description = "특정 볼링장 댓글을 삭제합니다.")
     @DeleteMapping("/{commentId}/delete")
     public ResponseEntity<String> deleteComment(@PathVariable Long centersId, @PathVariable Long commentId, @AuthenticationPrincipal User user) {
         String memberEmail = user.getUsername();
         centerCommentService.deleteComment(centersId, commentId, memberEmail);
-        return ResponseEntity.ok("레슨 삭제 성공");
+        return ResponseEntity.ok("댓글 삭제 성공");
     }
 }
--- src/main/java/com/capstone/bowlingbling/domain/comment/controller/LessonCommentController.java
+++ src/main/java/com/capstone/bowlingbling/domain/comment/controller/LessonCommentController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,8 +24,8 @@ public class LessonCommentController {
 
     @Operation(summary = "댓글 조회", description = "특정 레슨에 달린 모든 댓글을 조회합니다. 한 번에 10개씩 조회됩니다.")
     @GetMapping
-    public ResponseEntity<Page<CommentResponseDto>> getComments(@PathVariable Long lessonId) {
-        Page<CommentResponseDto> comments = lessonCommentService.getComments(lessonId, Pageable.ofSize(10));
+    public ResponseEntity<Page<CommentResponseDto>> getComments(@PathVariable Long lessonId, @RequestParam(defaultValue = "0") int page) {
+        Page<CommentResponseDto> comments = lessonCommentService.getComments(lessonId, PageRequest.of(page, 10));
         return ResponseEntity.ok(comments);
     }
 
@@ -41,14 +42,14 @@ public class LessonCommentController {
     public ResponseEntity<String> updateComment(@PathVariable Long lessonId, @PathVariable Long commentId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal User user) {
         String memberEmail = user.getUsername();
         lessonCommentService.updateComment(lessonId, commentId, requestDto, memberEmail);
-        return ResponseEntity.ok("레슨 수정 성공");
+        return ResponseEntity.ok("댓글 수정 성공");
     }
 
-    @Operation(summary = "댓글 삭제", description = "특정 마켓 댓글을 삭제합니다.")
+    @Operation(summary = "댓글 삭제", description = "특정 레슨 댓글을 삭제합니다.")
     @DeleteMapping("/{commentId}/delete")
     public ResponseEntity<String> deleteComment(@PathVariable Long lessonId, @PathVariable Long commentId, @AuthenticationPrincipal User user) {
         String memberEmail = user.getUsername();
         lessonCommentService.deleteComment(lessonId, commentId, memberEmail);
-        return ResponseEntity.ok("레슨 삭제 성공");
+        return ResponseEntity.ok("댓글 삭제 성공");
     }
 }
